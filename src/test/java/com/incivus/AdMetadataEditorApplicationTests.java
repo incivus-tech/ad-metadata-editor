161ef0944d8ddfad5f897f128d1721c0079deab4
incivus-tech <incivustech@incivus.ai>
2022-10-08 01:51:14 +0530

    Initial commit

---
+++ src/test/java/com/incivus/AdMetadataEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incivus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdMetadataEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
